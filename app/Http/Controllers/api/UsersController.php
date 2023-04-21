1b06038d7798cc21d7f6d1d66917475a1b1d8450
João Vitor <joaovitorb2345@gmail.com>
2023-04-21 12:42:50 -0300

    init

---
+++ app/Http/Controllers/api/UsersController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        User::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return User::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+    }
+}
--- controllers/cadastrar.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$_SESSION['id'] == 1;
-http_response_code(200);
-
-?>
